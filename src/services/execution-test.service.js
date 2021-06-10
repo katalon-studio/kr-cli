a9b8cee4759e3d26c705d52f0404db1ba586949b
Tranguyen-96 <tra.nguyen@katalon.com>
2021-06-10 13:49:32 +0700

    udpate

--- src/services/execution-test.service.js
+++ src/services/execution-test.service.js
@@ -11,33 +11,37 @@ const { openBrowser } = require("../helpers/run-browser.helpers");
 const { socketExecution } = require("../helpers/socket-execution.helper")
 
 const executionTestService = async function(browser, path, options) {
-    let dirname = pathLib.resolve(`./${path}`);
-
     try {
-        if (checkExistsFile(dirname) && checkFormatedFile(dirname) && checkKRformatedFile(dirname)) {
-            if (options.data) {
-                if (options.report && checkExistsFile(pathLib.resolve(`./${options.report}`))) {
-                    let dataMap = options.data.split(',').map(el => {
-                        return {
-                            name: el.split('/').pop(),
-                            dirname: pathLib.resolve(`./${el}`)
-                        };
-                    });
+        if (browser && path) {
+            let dirname = pathLib.resolve(`./${path}`);
+            if (checkExistsFile(dirname) && checkFormatedFile(dirname) && checkKRformatedFile(dirname)) {
+                if (options.data) {
+                    if (options.report && checkExistsFile(pathLib.resolve(`./${options.report}`))) {
+                        let dataMap = options.data.split(',').map(el => {
+                            return {
+                                name: el.split('/').pop(),
+                                dirname: pathLib.resolve(`./${el}`)
+                            };
+                        });
 
-                    if (dataMap.every(el => checkExistsFile(el.dirname)) == true && checkDataFilesinHTML(dirname, dataMap) == true) {
-                        return openBrowser(browser)
-                            .then((driver) => socketExecution(driver, dirname, dataMap, options.report));
-                    } else {
-                        return openBrowser(browser)
-                            .then((driver) => socketExecution(driver, dirname, undefined, options.report));
+                        if (dataMap.every(el => checkExistsFile(el.dirname)) == true && checkDataFilesinHTML(dirname, dataMap) == true) {
+                            return openBrowser(browser)
+                                .then((driver) => socketExecution(driver, dirname, dataMap, options.report));
+                        } else {
+                            return openBrowser(browser)
+                                .then((driver) => socketExecution(driver, dirname, undefined, options.report));
+                        }
                     }
+                } else {
+                    log("The path of ReportLog is not valid. Please try again!", true);
                 }
             } else {
-                log("The path of ReportLog is not valid. Please try again!", true);
+                log("The path is not valid. Please try again!", true);
             }
         } else {
-            log("The path is not valid. Please try again!", true);
+            log("Browser or path is not valid. Please try again!", true);
         }
+
     } catch (error) {
         log(`Error: ${error}`, true);
         throw error;
