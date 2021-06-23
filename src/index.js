b917307885e0e52127f95e4b299c5c9e72b5edcd
Tranguyen-96 <tra.nguyen@katalon.com>
2021-06-23 16:23:34 +0700

    Update

--- src/index.js
+++ src/index.js
@@ -8,18 +8,18 @@ global.appRoot = path.resolve(__dirname);
 program.version('1.0.0').description('Hello I am Katalon Recorder CLI')
 
 program
-    .command('run <browser> [path]')
     .option('-d , --data [dataFiles]', 'data files')
     .option('-rp , --report [reportDir]', 'report files')
+    .command('run <browser> [path]')
     .alias('-r')
     .description('Run test suite')
     .action((browser, path, options) => executionTestController(browser, path, options))
 
 program
+    .option('-lg , --logger', 'logger')
     .command('dev <browser>')
     // .option('-p , --path [path]', 'specified path')
     // .option('-d , --data [dataFiles]', 'data files')
-    .option('-lg , --logger', 'logger')
     .alias('-dev')
     .description('Run test suite in development')
     .action((browser, options) => executionTestDevController(browser, options))
